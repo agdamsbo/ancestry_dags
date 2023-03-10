25651e45949a6f2d153d960625d3979654b47e8a
AG Damsbo <agdamsbo@pm.me>
2023-03-10 11:43:06 +0100

    renaming functions and minimal documentation

--- R/ancestor.R
+++ R/ancestor.R
@@ -1,18 +1,19 @@
-#' @title Calculate Ancestor Probability
-#' @description Calculates the probability of an ancestor given a population size and previous probability.
+#' Simulate single ancestor
+#' @description Simulates an ancestor given a population size and prevalence.
 #' @param population The population size.
 #' @param prev The previous probability.
 #' @return A vector of probabilities.
-#' @export
 #'
 #' @examples
-#' ancestor(population = 10000, prev = 0.1)
-ancestor <- function(population = 10000, prev = 0.1) {
+#' ancestor.s(population = 10000, prev = 0.1)
+ancestor.s <- function(population = 10000, prev = 0.1) {
   rbinom(population, 1, prev)
 }
 
 #' Simulate multiple ancestors
 #'
+#' @description Simulates multiple ancestors given population size and
+#' prevalence(s).
 #' @param population Population
 #' @param prev Prevalence of outcome in ancestor(s). If length is 1, the
 #' value is reused for all.
@@ -22,19 +23,20 @@ ancestor <- function(population = 10000, prev = 0.1) {
 #' @export
 #' @examples
 #' pop <- 10000
-#' ancestor.multi(population = pop, prev = 0.1, n = 3)
-#' ancestor.multi(population = pop, prev = c(0.1, 0.3, 0.2), anc.names = c("anc2", "anc3", "anc4"))
-ancestor.multi <- function(population, prev, n = 1, anc.names = NULL) {
+#' ancestor(population = pop, prev = 0.1, n = 3)
+#' ancestor(population = pop, prev = c(0.1, 0.3, 0.2),
+#' anc.names = c("anc2", "anc3", "anc4"))
+ancestor <- function(population, prev, n = 1, anc.names = NULL) {
   if (is.null(anc.names)) {
     if (length(prev) != n) prev <- rep(prev, n)
     l <- lapply(seq_len(n), function(i) {
-      ancestor(population = population, prev = prev[i])
+      ancestor.s(population = population, prev = prev[i])
     })
     names(l) <- paste0("ancestor", seq_len(n))
   } else {
     if (length(prev) != length(anc.names)) prev <- rep(prev, length(anc.names))
     l <- lapply(seq_along(anc.names), function(i) {
-      ancestor(population = population, prev = prev[i])
+      ancestor.s(population = population, prev = prev[i])
     })
     names(l) <- anc.names
   }
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # ancestryDAGs
 
 Package with simple functions to quantify DAGs in R. Based on course mmaterial from PhD course on Directed Acyclic Graphs (DAG) at AU.
+
+## Functions
+
+The package includes four functions:
+
+* `ancestor()`: Simulates multiple ancestors given a population size and prevalences.
+* `child()`: Simulates child given a number of parents and odds ratios for the relations.
