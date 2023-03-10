917a68c048498916662b66b6b1801080e2b62711
AG Damsbo <agdamsbo@pm.me>
2023-03-10 11:29:07 +0100

    styling

--- R/ancestor.R
+++ R/ancestor.R
@@ -1,5 +1,3 @@
-
-
 #' @title Calculate Ancestor Probability
 #' @description Calculates the probability of an ancestor given a population size and previous probability.
 #' @param population The population size.
@@ -8,8 +6,8 @@
 #' @export
 #'
 #' @examples
-#' ancestor(population=10000, prev=0.1)
-ancestor <- function(population=10000,prev=0.1){
+#' ancestor(population = 10000, prev = 0.1)
+ancestor <- function(population = 10000, prev = 0.1) {
   rbinom(population, 1, prev)
 }
 
@@ -23,19 +21,21 @@ ancestor <- function(population=10000,prev=0.1){
 #' @return List of ancestor objects
 #' @export
 #' @examples
-#' pop=10000
-#' ancestor.multi(population=pop,prev=0.1,n=3)
-#' ancestor.multi(population=pop,prev=c(0.1,0.3,0.2),anc.names=c("anc2","anc3","anc4"))
-ancestor.multi <- function(population,prev,n=1,anc.names=NULL){
-  if (is.null(anc.names)){
-    if (length(prev)!=n) prev <- rep(prev,n)
-    l <- lapply(seq_len(n),function(i){
-      ancestor(population=population,prev=prev[i])})
-    names(l) <- paste0("ancestor",seq_len(n))
+#' pop <- 10000
+#' ancestor.multi(population = pop, prev = 0.1, n = 3)
+#' ancestor.multi(population = pop, prev = c(0.1, 0.3, 0.2), anc.names = c("anc2", "anc3", "anc4"))
+ancestor.multi <- function(population, prev, n = 1, anc.names = NULL) {
+  if (is.null(anc.names)) {
+    if (length(prev) != n) prev <- rep(prev, n)
+    l <- lapply(seq_len(n), function(i) {
+      ancestor(population = population, prev = prev[i])
+    })
+    names(l) <- paste0("ancestor", seq_len(n))
   } else {
-    if (length(prev)!=length(anc.names)) prev <- rep(prev,length(anc.names))
-    l <- lapply(seq_along(anc.names),function(i){
-      ancestor(population=population,prev=prev[i])})
+    if (length(prev) != length(anc.names)) prev <- rep(prev, length(anc.names))
+    l <- lapply(seq_along(anc.names), function(i) {
+      ancestor(population = population, prev = prev[i])
+    })
     names(l) <- anc.names
   }
   l
--- R/child.R
+++ R/child.R
@@ -1,5 +1,3 @@
-
-
 #' @title Calculate the probability of a child with given parameters
 #' @param baserisk A numeric vector containing the base risk
 #' @param por A list containing the odds ratios
@@ -9,21 +7,25 @@
 #' @export
 #'
 #' @examples
-#' pop=10000
-#' child(baserisk = 0.1,
-#'      por = list(1.2, 0.8),
-#'      parent = ancestor.multi(population=pop,prev=0.2,n=2))
+#' pop <- 10000
+#' child(
+#'   baserisk = 0.1,
+#'   por = list(1.2, 0.8),
+#'   parent = ancestor.multi(population = pop, prev = 0.2, n = 2)
+#' )
 #' @export
-child <- function(baserisk,por,parent){
-  if (!"list"%in%class(por)){
-    l <- list("por"=list(por),"par"=list(parent))
-  } else l <- list("por"=por,"par"=parent)
+child <- function(baserisk, por, parent) {
+  if (!"list" %in% class(por)) {
+    l <- list("por" = list(por), "par" = list(parent))
+  } else {
+    l <- list("por" = por, "par" = parent)
+  }
 
-  list.pars <- lapply(seq_len(length(l$por)),function(i){
-    log(l$por[[i]])*l$par[[i]]
+  list.pars <- lapply(seq_len(length(l$por)), function(i) {
+    log(l$por[[i]]) * l$par[[i]]
   })
 
-  myxb1 <- log(baserisk/(1-baserisk)) + do.call("+",list.pars)
-  probp1 = exp(myxb1)/(1 + exp(myxb1))
+  myxb1 <- log(baserisk / (1 - baserisk)) + do.call("+", list.pars)
+  probp1 <- exp(myxb1) / (1 + exp(myxb1))
   rbinom(length(l$par[[1]]), 1, probp1)
 }
